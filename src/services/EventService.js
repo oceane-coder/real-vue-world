d0f289494d0e8d03cdceaebc55cd07341fe6a63b
oceane_coder <60930095+oceane-coder@users.noreply.github.com>
2021-04-27 15:21:28 +0200

    Add files via upload

---
+++ src/services/EventService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/oceane-coder/demo',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+export default {
+  getEvents() {
+    return apiClient.get('/events')
+  },
+  //Added new call
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
+  },
+}
